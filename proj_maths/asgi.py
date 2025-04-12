16b4d7b1cbf42958fdbe42eaab26c9d7b4ce74a2
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-12 15:54:13 +0300

    german_asgi.py

--- proj_maths/asgi.py
+++ proj_maths/asgi.py
@@ -1,7 +1,7 @@
 """
-ASGI config for proj_maths project.
+ASGI config for proj_german project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named application.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj_maths.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj_german.settings')
 
 application = get_asgi_application()
